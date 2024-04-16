fe58632b9be52dbde740e4dcc5952ee275cbc039
LukaAlhonen <luka.alhonen@protonmail.com>
2024-04-16 19:28:47 +0300

    Finnished socket programming and cleaned up code

--- root/enter.py
+++ root/enter.py
@@ -9,58 +9,197 @@ import pandas as pd
 import socket
 import pickle
 from pprint import pprint
+import json
+import threading
+from dotenv import load_dotenv
+import time
+import random
+import base64
 
-if __name__ == "__main__":
-    # Path of test file should be transferred by arguments in sys.argv or from environment variable 'batch_path'
-    args = sys.argv
-    if 'batch_path' in os.environ:
-        path = os.environ['batch_path']
-    elif len(args) == 2:
-        path = args[1]
-    else:
-        raise ValueError("Please provide 1 absolute path of the batch!")
-    
-    df = pd.read_parquet(path)
-    pids = schedule(df)
+class Worker:
+    def __init__(self, address: str, port: int):
+        self.address = address
+        self.port = port
+        self.is_busy = False
 
-    if pids == "alarm":
-        sys.exit(-1)
+# List of worker nodes
+workers = []
 
-    serialized_data = task_creation(pids, df)
+lock = threading.Lock()
 
-    # Socket Programming part - Transfer serialized_data from Root to Worker
+def get_worker_resources(worker: Worker) -> tuple[str, str]:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if 'worker_address' not in os.environ and 'worker_port' not in os.environ:
-        print('No address or port specified, binding to "localhost:8001"')
-        address = 'localhost' # Replace with worker IP
-        port = 8001
-    else:
-        address = os.environ['worker_address']
-        port = os.environ['worker_port']
-    s.connect((address, port))
-    print(f'Connected to {address}:{port}')
-    print('Sending serilazied data...')
-    s.send(serialized_data)
-    s.shutdown(socket.SHUT_WR) # Close sending side of socket
-
-    # Wait for response from worker
-    response = b''
-    print('Waiting for response...')
+    s.connect((worker.address, worker.port))
+    message = {'type': 'request_resources'}
+    s.send(json.dumps(message).encode())
+    s.shutdown(socket.SHUT_WR)
+    s.settimeout(60)
     try:
+        chunks = b''
         while True:
             chunk = s.recv(1024)
             if not chunk:
                 break
-            response += chunk
+            chunks += chunk
+        resources = json.loads(chunks.decode())
+        s.close()
+        return (resources['cpus'], resources['memory'])
+    except socket.timeout:
+        print(f'Worker {worker.address} timed out')
+
+def get_eligible_workers() -> list[Worker]:
+    # Get all registered workers that are not busy
+    global workers
+    with lock:
+        eligible_workers = []
+        for worker in workers:
+            if not worker.is_busy:
+                eligible_workers.append(worker)
+            else:
+                print(f'Worker {worker.address} is busy')
+
+    return eligible_workers
+
+def send_job(job, worker: Worker) -> any:
+    # Send job to worker and get result of job
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    response = ''
+    print(f'Sending job to worker {worker.address}')
+    s.connect((worker.address, worker.port))
+    serialised_data = base64.b64encode(pickle.dumps(job)).decode()
+    message = {'type': 'job', 'data': serialised_data}
+    s.send(json.dumps(message).encode())
+    s.shutdown(socket.SHUT_WR)
+    worker.is_busy = True
+    s.settimeout(600)
+    try:
+        chunks = b''
+        while True:
+            chunk = s.recv(1024)
+            if not chunk:
+                break
+            chunks += chunk
+        response = json.loads(chunks.decode())
+        worker.is_busy = False
+        if response['type'] == 'job':
+            serialised_response = base64.b64decode(response['result'])
+            deserialised_data = pickle.loads(serialised_response)
+            return deserialised_data
     except:
-        print('Something unexpected occured...')
-    
-    # Deserialise response and print rows containing anomalies
-    deserialised_response = pickle.loads(response)
-    print(deserialised_response)
-    for idx in deserialised_response:
-        pprint(df.iloc[idx].to_dict())
-    print('Done.')
+        print('Something went wrong')
     s.close()
-    
+
+    return None
+
+
+def schedule_job(job: any) -> any:
+    response = None
+    eligible_workers = get_eligible_workers()
+    target_worker = None
+    target_cpus = 0
+    target_memory = 0
+
+    # get worker with most resources
+    if len(eligible_workers) > 0:
+        for worker in eligible_workers:
+            cpus, memory = get_worker_resources(worker)
+            print(f'Worker {worker.address} has {cpus} cpus and {memory} MB free memory')
+            if cpus >= target_cpus and memory >= target_memory:
+                target_cpus = cpus
+                target_memory = memory
+                target_worker = worker
+        response = send_job(job, target_worker)
+    return response
+
+def handle_worker_register(worker_address: str, worker_port: int) -> bool:
+    isregistered = False
+    new_worker = Worker(worker_address, int(worker_port))
+    success = False
+    global workers
+    with lock:
+        if len(workers) > 0:
+            for worker in workers:
+                if worker.address == worker_address:
+                    isregistered = True
+        if not isregistered:
+            workers.append(new_worker)
+            success = True
+
+    return success
+
+def listen_for_registration(s: socket, stop_event: threading.Event) -> None:
+    s.settimeout(10) # This is set to properly handle KeyboardInterrupt
+    while not stop_event.is_set():
+        try:
+            c, addr = s.accept()
+            chunks = b''
+            while True:
+                chunk = c.recv(1024)
+                if not chunk:
+                    break
+                chunks += chunk
+            message = json.loads(chunks.decode())
+            if message['type'] == 'worker_register':
+                worker_address = addr[0]
+                worker_port = message['worker_port']
+                if handle_worker_register(worker_address, worker_port):
+                    print(f'Worker {worker_address} successfully registered')
+                    response = {'type': 'worker_register', 'success': True}
+                else:
+                    response = {'type': 'worker_register', 'success': False}
+                c.send(json.dumps(response).encode())
+                c.shutdown(socket.SHUT_WR)
+                
+        except socket.timeout:
+            continue
+
+def get_random_traffic_set():
+    dir = 'test_set'
+    files = os.listdir(dir)
+    random_file = random.choice(files)
+    return os.path.join(dir, random_file)
+
+def get_job():
+    path = get_random_traffic_set()
+    df = pd.read_parquet(path)
+    pids = schedule(df)
+    return (pids, df)
+
+if __name__ == "__main__":
+    load_dotenv() # Load env vars
+
+    try: 
+        # Start listening
+        port = int(os.environ['ROOT_PORT'])
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', port))
+        s.listen(1)
+        print(f'Socket is listening on port: {port}')
+        stop_event = threading.Event()
+        listen_thread = threading.Thread(target=listen_for_registration, args=(s,stop_event))
+        listen_thread.start()
+
+        # Send a random job to a worker every 10 seconds
+        while True:
+            if len(workers) > 0:
+                print('Scheduling job')
+                pids, df = get_job()
+                if pids == 'alarm':
+                    continue
+                serialized_data = task_creation(pids, df)
+                result = schedule_job(serialized_data)
+                if not result is None:
+                    print(result)
+                    for idx in result:
+                        pprint(df.iloc[idx].to_dict())
+                    print('Done.')
+            time.sleep(10)
+
+
+    except KeyboardInterrupt:
+        print('\nExiting...')
+        stop_event.set()
+        listen_thread.join()
+        s.close()
+        sys.exit(1)
     
